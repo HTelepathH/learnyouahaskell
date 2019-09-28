2ad24d6e3c8ca8c3e140721788eb0ba31c9a2744
HuangH <99088577@qq.com>
2019-09-28 21:25:01 +0800

    recursive

---
+++ recursive/solveRPN.hs
@@ -0,0 +1,22 @@
+import Data.List
+import Control.Monad
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x,"")] -> Just x
+                                _ -> Nothing
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do 
+    [result] <- foldM foldingFunction [] (words st)
+    return result
+
+foldingFunction :: [Double] -> String -> Maybe [Double]  
+foldingFunction (x:y:ys) "*" = return ((x * y) : ys)
+foldingFunction (x:y:ys) "/" = return ((y / x) : ys)
+foldingFunction (x:y:ys) "+" = return ((x + y) : ys)
+foldingFunction (x:y:ys) "-" = return ((y - x) : ys)
+foldingFunction (x:y:ys) "^" = return ((y ** x) : ys) 
+foldingFunction (x:xs) "ln" = return (log x : xs)  
+foldingFunction xs "sum" = return ([sum xs] )
+foldingFunction xs numberString = liftM (:xs) $ readMaybe numberString
+
