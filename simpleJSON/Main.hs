a3ae1c022d8d481997c17b11f70096c2c03bbdc5
HuangH <99088577@qq.com>
2019-12-02 16:50:12 +0800

    simpleJSON

---
+++ simpleJSON/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import SimpleJSON
+import PrettyJSON
+import Prettify
+
+import Prelude hiding ((<>))
+
+result :: JValue
+result = JObject [
+  ("query", JString "awkward squad haskell"),
+  ("estimatedCount", JNumber 3920),
+  ("moreResults", JBool True),
+  ("results", JArray [
+     JObject [
+      ("title", JString "Simon Peyton Jones: papers"),
+      ("snippet", JString "Tackling the awkward ..."),
+      ("url", JString "http://.../marktoberdorf/")
+     ]])
+  ]
+
+main = putStrLn $ pretty 11. nest 4. renderJValue $ result
